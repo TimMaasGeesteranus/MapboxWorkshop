d478d3fa036751491af2fb0200054602dcbf44a8
Tim Maas Geesteranus <tim__mg@hotmail.com>
2020-05-07 19:17:54 +0200

    add project

---
+++ InsideAirBNB/InsideAirBNB/Models/SummaryReviews.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InsideAirBNB.Models
+{
+    public partial class SummaryReviews
+    {
+        public int ListingId { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
